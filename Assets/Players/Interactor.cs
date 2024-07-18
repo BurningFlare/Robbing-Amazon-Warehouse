1dd06c9fc1daa31c2a2dc05163a67efc350c8ff1
The64thRealm <36716817+The64thRealm@users.noreply.github.com>
2024-07-18 00:23:12 -0700

    Added some things for interaction, still wip
    Interactor component searches for interactable items
    interactable interface
    added prop component
    
    none of these are very complete lol

---
+++ Assets/Players/Interactor.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactor : MonoBehaviour
+{
+    [SerializeField] public Transform interactionPoint;
+    [SerializeField] private float interactionRadius = 1f;
+    [SerializeField] private LayerMask interactionMask;
+
+    private readonly Collider2D[] interactableColliders = new Collider2D[3];
+    [SerializeField] private int numFound;
+
+    private void Update()
+    {
+        numFound = Physics2D.OverlapCircleNonAlloc(interactionPoint.position, interactionRadius, interactableColliders, interactionMask);
+        Debug.Log(interactionPoint.position);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(interactionPoint.position, interactionRadius);
+    }
+}
