91110f1f0f70832ac479ed330e59fdbbe27ade64
The64thRealm <36716817+The64thRealm@users.noreply.github.com>
2024-07-28 23:07:18 -0700

    Collected merch is now a string set
    because I removed all derivative merch types, we store them as strings now

--- Assets/Level/GameManager.cs
+++ Assets/Level/GameManager.cs
@@ -1,6 +1,7 @@
 using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -8,13 +9,15 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance;
     [SerializeField] public Player player;
     [SerializeField] private GameObject gameOverPanel;
-    
+
+    private List<MerchBase> merch;
+
     private Interactor interactor;
     public Animator cameraAnimator;
 
     public float totalFunds = 0;
     public float currentRent = 0;
-    public HashSet<System.Type> collectedMerch = new HashSet<System.Type>();
+    public HashSet<string> collectedMerch = new HashSet<string>();
 
     private bool gameOver = false;
 
@@ -24,9 +27,17 @@ public class GameManager : MonoBehaviour
         if (Instance == null) { Instance = this; }
         else { Destroy(this); }
         cameraAnimator = Camera.main.GetComponent<Animator>();
+        generateMerch();
         // TODO get the player somehow idk
 
         interactor = player.GetComponent<Interactor>();
+    }
+
+    private void generateMerch()
+    {
+        // getting all merch that is in the game for some reason
+        merch = new List<MerchBase>(Resources.FindObjectsOfTypeAll<MerchBase>());
+        // TODO actually generate the merch
     }
 
     private void Update()
@@ -80,10 +91,26 @@ public class GameManager : MonoBehaviour
         // TODO animation and scene transition
         foreach (MerchBase merchItem in player.inventory.merchList)
         {
-            collectedMerch.Add(merchItem.GetType());
+            // delete the numbering added by unity
+            Match match = Regex.Match(merchItem.name, @"^(.*) \([0-9]+\)$");
+            if (match.Success)
+            {
+                collectedMerch.Add(match.Groups[1].Value);
+            } else
+            {
+                collectedMerch.Add(merchItem.name);
+            }
         }
         totalFunds += player.inventory.totalCost;
         player.inventory.clear();
+
+        string items = "";
+        foreach (string itemName in collectedMerch)
+        {
+            items += itemName + " ";
+        }
+        Debug.Log(items);
+        Debug.Log(totalFunds);
         return true;
     }
 }
